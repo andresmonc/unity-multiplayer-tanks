6048cc85393ae4eaee7b3d336759afed06a71411
andresmonc <jaimeamonc@gmail.com>
2024-05-29 00:12:01 -0500

    add image ref

--- Assets/Scripts/Core/Combat/HealingZone.cs
+++ Assets/Scripts/Core/Combat/HealingZone.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using Unity.Netcode;
 using UnityEngine;
 
-public class HealingZone : MonoBehaviour
+public class HealingZone : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Image healPowerBar;
+    
 }
