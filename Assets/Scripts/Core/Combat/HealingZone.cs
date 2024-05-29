872785477e8a5520d3ac941bd7e907c668424696
andresmonc <jaimeamonc@gmail.com>
2024-05-29 00:14:12 -0500

    add more settings

--- Assets/Scripts/Core/Combat/HealingZone.cs
+++ Assets/Scripts/Core/Combat/HealingZone.cs
@@ -9,5 +9,11 @@ public class HealingZone : NetworkBehaviour
     [Header("References")]
     [SerializeField] private Image healPowerBar;
     [Header("Settings")]
-    [SerializeField] private int maxHealPower;
+    [SerializeField] private int maxHealPower = 30;
+    [SerializeField] private float healCooldown = 30f;
+    [SerializeField] private float healTickRate = 1f;
+    [SerializeField] private int coinsPerTick = 1;
+    [SerializeField] private int healthPerTick = 1;
+
+
 }
